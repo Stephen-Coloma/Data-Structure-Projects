6123657e21feb9c14e5c5b9671edafce22abef8e
Hannah Ragudos <2233361@slu.edu.ph>
2023-10-12 18:54:00 +0800

    initial commit: Implement PostfixEvaluatorTester with basic error handling and user interaction

--- src/midlab1/PostfixEvaluatorTester.java
+++ src/midlab1/PostfixEvaluatorTester.java
@@ -1,24 +1,41 @@
 package midlab1;
 
 import midlab1.stack.StackUnderflowException;
-
 import java.util.Scanner;
 
 public class PostfixEvaluatorTester {
+
+    /*
+    Algorithm for Evaluating a Postfix Expression:
+
+    1. Create an empty stack for storing operands.
+    2. For each character in the postfix expression string:
+        - If it's a digit, push it onto the stack.
+        - If it's an operator (+, -, *, /, ^):
+            a. Pop the top two operands from the stack.
+            b. Apply the operator and push the result back onto the stack.
+    3. The final result should be the only element left on the stack.
+    4. Pop and return it as the evaluated result.
+    */
     public static void main(String[] args) {
         PostfixEvaluator postfixEvaluator;
         Scanner scanner = new Scanner(System.in);
 
-        //System.out.print("Enter a postfix expression (operands and operators separated by space): ");
-        String postfixExpression = "6 5 * 7 + 8 *";
+        System.out.print("Enter a postfix expression (operands and operators separated by space): ");
+        String postfixExpression = scanner.nextLine();
 
         try {
             postfixEvaluator = new PostfixEvaluator(postfixExpression);
             int result = postfixEvaluator.evaluate(postfixExpression);
-            System.out.print("Result: " +result);
-        } catch (ArithmeticException e) {
+            System.out.print("Result: " + result);
+        }
+        catch (ArithmeticException e) {
             System.out.println("Error: " + e.getMessage());
-        } catch(Exception e1){
+        }
+        catch (StackUnderflowException e) {
+            System.out.println("Stack Underflow Error: " + e.getMessage());
+        }
+        catch (Exception e1) {
             e1.printStackTrace();
         }
     }
