2637664461f04a1753398c2a1016e3159e6a47aa
Stephen Coloma <2232847@slu.edu.ph>
2023-10-12 20:16:40 +0800

    add: convertToPostFix method is already working

--- src/midlab1/InfixConverter.java
+++ src/midlab1/InfixConverter.java
@@ -4,10 +4,11 @@
 
 package midlab1;
 
+import midlab1.stack.MyStack;
+
 //TODO: Create javadoc comments
 public class InfixConverter {
     private String infix;
-    private String postfix;
 
     public InfixConverter(String infix) {
         this.infix = infix;
@@ -21,9 +22,43 @@ public class InfixConverter {
         this.infix = infix;
     }
 
-    public void convertToPostfix(){
-        //TODO: create implementation of the conversion of the infix. Copy the code of sir Dalos
-        //TODO: Make sure that invalid input (errors) can be handled.
+    public String convertToPostfix(){
+         infix = infix.replaceAll(" ", "");
+         int index = 0;
+
+         String postfixExpression = "";
+         MyStack<Character> operatorStack = new MyStack<>();
+
+         try {
+             while (index!=infix.length()){
+                 char symbol = infix.charAt(index);
+                 if (isOperand(symbol)){
+                     postfixExpression+=symbol;
+                 }else {
+                     while (!operatorStack.isEmpty() && precedence(operatorStack.peek(), symbol)){
+                         char topSymbol = operatorStack.pop();
+                         postfixExpression+=topSymbol;
+                     }
+
+                     if (operatorStack.isEmpty() || symbol  != ')'){
+                         operatorStack.push(symbol);
+                     }
+                     else{
+                         char topSymbol = operatorStack.pop();
+                     }
+                 }
+                 index++;
+             }
+
+             while (!operatorStack.isEmpty()) {
+                 char topSymbol = operatorStack.pop();
+                 postfixExpression+=topSymbol;
+             }
+         }catch (Exception e){
+             e.printStackTrace();
+         }
+
+         return postfixExpression;
     }
 
     private static boolean precedence(char operator1, char operator2){
@@ -38,16 +73,31 @@ public class InfixConverter {
         else if ((operator1 == '+' || operator1 == '-') && (operator2 == '+' || operator2 == '-'))
             return true;
         else if (operator2 == ')'){
+            if (operator1 == '('){
+                return false;
+            }
             return true;
         }else
             return false;
     }
 
+    private boolean isOperand(char symbol){
+        char[] operators = {'^', '(', ')', '*', '/', '+', '-'};
+        for (char operator:operators) {
+            if (operator==symbol){
+                return false;
+            }
+        }
+        return true;
+    }
+
     @Override
     public String toString() {
-        return "InfixConverter{" +
-                "infix='" + infix + '\'' +
-                ", postfix='" + postfix + '\'' +
-                '}';
+        return infix;
+    }
+
+    public static void main(String[] args) {
+        InfixConverter hi = new InfixConverter("(A+B/C*(D+E)-F)");
+        System.out.println(hi.convertToPostfix());
     }
 }
