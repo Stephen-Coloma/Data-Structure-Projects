1c9cf1053220baee51a211cc0dcb194ccbf85c1d
Hannah Ragudos <2233361@slu.edu.ph>
2023-10-14 22:01:20 +0800

    feat: refactor PostfixEvaluatorTester for enhanced user interaction
    - Improve user prompts and feedback for clearer and friendlier interaction in `run` method.
    - Simplify exception handling for cleaner code structure and efficient error tracking.
    - Ensure the application loops until the user opts to exit, enhancing user experience.
    - Update class and method documentation to reflect changes and clarify usage.

--- src/Midterms/Project1/PostfixEvaluatorTester.java
+++ src/Midterms/Project1/PostfixEvaluatorTester.java
@@ -15,7 +15,7 @@
 
 /*
 SAMPLE RUN:
-    Please enter a postfix expression.
+Please enter a postfix expression.
 Enter postfix expression: 6 2 3 + - 3 8 2 / + * 2 $ 3 +
 Postfix String -> 6 2 3 + - 3 8 2 / + * 2 $ 3 +
 +--------+----------+----------+-------+-----------------+
